f805f467f7f12a052118e2046f090fa6226be770
欧拉木槿 <770069989@qq.com>
2024-07-17 09:48:29 +0800

    feat：基础类型

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+// const str:string = 'aa';
+// const num:number = 123;
+// const str:string = `${num}`;
+// console.log(str);
+// let notANumber: number = NaN;//Nan
+// let num: number = 123;//普通数字
+// let infinityNumber: number = Infinity;//无穷大
+// let decimal: number = 6;//十进制
+// let hex: number = 0xf00d;//十六进制
+// let binary: number = 0b1010;//二进制
+// let octal: number = 0o744;//八进制
+// let b1: Boolean = true;
+// let b2: Boolean = false;
+// let n: null = null;
+// let b: undefined = undefined;
+// 如果是严格模式，则null会报错。此时可以去tsconfig.json将 strict 设置为 false，来关闭严格模式
+// let v1: void = null
+// let v2: void = undefined
+// 当处于严格模式下时，null 和 undefied 不能互相赋值
+// let n: null = null
+// let a: undefined = undefined
+// n = a
+// a = n
+// 当函数的返回值设置为 void 时，函数体内部不能有返回值
+function myFn() {
+    return;
+}
